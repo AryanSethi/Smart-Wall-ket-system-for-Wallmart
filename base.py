0d8f1cee7993c907a8eff613077da5a15da33985
Aryan Sethi <46836282+AryanSethi@users.noreply.github.com>
2020-04-24 21:53:20 +0530

    Add files via upload

--- base.py
+++ base.py
@@ -1,16 +1,17 @@
 import pygame
+import sys
 
 categories = {
-    "Meat and Poultry": (1,1),
-    "Soft Drinks": (1,19),
-    "Alcohol and spirits": (4,6),
-    "Home Items": (9,4),
-    "Stationary": (16,6),
-    "Sea Food": (13,14),
-    "Bakery": (11,14),
-    "Frozen": (20,11),
-    "Grocery": (20,16),
-    "Florist": (10,1)
+    "Meat and Poultry": (1, 1),
+    "Soft Drinks": (1, 19),
+    "Alcohol and spirits": (4, 6),
+    "Home Items": (9, 4),
+    "Stationary": (16, 6),
+    "Sea Food": (13, 14),
+    "Bakery": (11, 14),
+    "Frozen": (20, 11),
+    "Grocery": (20, 16),
+    "Florist": (10, 1)
 }
 
 sub_categories = {
@@ -53,7 +54,7 @@ all_orders = [
             {
                 "description": "Britania eggless cakes",
                 "quantity": 10
-            },            {
+            }, {
                 "description": "Eggs",
                 "quantity": 15
             },
@@ -83,9 +84,8 @@ all_orders = [
     },
 ]
 
-batched_orders =[]
-destinations=[]
-
+batched_orders = []
+destinations = []
 
 for order in all_orders:
     for products in order["products"]:
@@ -95,84 +95,64 @@ for product in batched_orders:
     if categories[sub_categories[product]] not in destinations:
         destinations.append(categories[sub_categories[product]])
 
-WIDTH = 22
-HEIGHT = 22
-BLACK = (0, 0, 0)
-BLUE=(0,0,255)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-MARGIN = 2
-grid = []
-for row in range(21):
-    grid.append([])
-    for column in range(21):
-        grid[row].append(0)
 
-# designing the map of one store
-                        ##############################
-for i in range(17):
-    grid[i][0]=4
-    grid[i][1]=4     #sides
-    grid[i][-1]=4
-    grid[i][-2]=4
-for i in range(2):
-    for j in range(3,18):   #top and bottom
-        grid[i][j]=4
-        grid[-1][j]=4
-for i in range(4,7):
-    for j in range(4,14):
-        grid[j][i]=4         #middle
-        grid[j][i+5]=4
-        grid[j][i + 10] = 4
-for i in range(16,18):
-    for j in range(4,7):
-        grid[i][j] = 4      #below middle
-        grid[i][j+5] = 4
-        grid[i][j+10] = 4
-
-        ##############################
-# this marks all the journey destinations we have to cover
-for destination in destinations:
-    grid[destination[0]][destination[1]]=1
 
 pygame.init()
-WINDOW_SIZE = [510, 520]
+WINDOW_SIZE = [850, 850]
+vector = pygame.math.Vector2
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Store Map")
 done = False
 clock = pygame.time.Clock()
+background_image = pygame.image.load("new.jpg")
+
+tilesize=10
+gridwidth=800
+gridheight=770
+BLACK=(1,1,1)
+WHITE=(255,255,255)
+GRAY=(140,140,140)
+
+
+class Grid:
+    def __init__(self,width,height):
+        self.width=width
+        self.height= height
+        self.walls = []
+        self.connections = [vector(1,0),vector(0,1),vector(-1,0),vector(0,-1)]
+
+    def in_bounds(self,node):
+        return 0<= node.x < self.width and 0<= node.y <self.height
+
+    def passable(self,node):
+        return node not in self.walls
+
+    def find_neighbours(self,node):
+        neigbours = [node + connection for connection in self.connections]
+        neigbours= filter(self.in_bounds, neigbours)
+        neigbours = filter(self.passable, neigbours)
+        print(neigbours)
+
+    def draw_grid(self):
+        for x in range(0, self.width,tilesize):
+            pygame.draw.line(screen, GRAY, (x, 0), (x, self.height))
+        for y in range(0, self.height, tilesize):
+            pygame.draw.line(screen, GRAY, (0, y), (self.width, y))
+
+g= Grid(gridwidth,gridheight)
+g.walls=[vector(2,1),vector(2,2)]
+g.draw_grid()
+background_image.set_alpha(140)
+screen.blit(background_image, [0,0])
+g.find_neighbours((10,10))
+
 while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
-        # elif event.type == pygame.MOUSEBUTTONDOWN:
-        #     pos = pygame.mouse.get_pos()
-        #     column = pos[0] // (WIDTH + MARGIN)
-        #     row = pos[1] // (HEIGHT + MARGIN)
-        #     # Set that location to one
-        #     grid[row][column] = 3
+            done = True
 
-    screen.fill(BLACK)
 
-    # Draw the grid
-    for row in range(21):
-        for column in range(21):
-            color = WHITE
-            if grid[row][column] == 1:
-                color = RED
-            elif grid[row][column] == 2:
-                    color = GREEN
-            elif grid[row][column] == 3:
-                color = BLACK
-            elif grid[row][column] == 4:
-                color = BLUE
-            pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
+
 
     # Limit to 60 frames per second
     clock.tick(60)
